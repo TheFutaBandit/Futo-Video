c2e06e07790f87bbe231ccd774600a46b92db15d
TheFutaBandit <prameshanand2021@gmail.com>
2025-08-17 23:54:38 +0530

    working on change video component

--- src/components/forms/change-video-location.tsx/index.tsx
+++ src/components/forms/change-video-location.tsx/index.tsx
@@ -1,3 +1,5 @@
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 type PropType = {
@@ -18,7 +20,24 @@ const ChangeVideoLocation = ({
         <form className = "flex flex-col gap-y-5">
             <div className = "border-[1px] rounded-xl p-5">
                 <h2 className = "text-xs mb-5 text-[#a4a4a4]">Current</h2>
-                {workspace && <p className ="text-[#a4a4a4]"}
+                <p className = "text-[#a4a4a4]">Workspace</p>
+                <p className = "text-[#a4a4a4] text-sm">This video has no fodler</p>
+            </div>
+            <Separator orientation="horizontal" />
+            <div className = "flex flex-col gap-y-5 border-[1px] rounded-xl">
+                <h2 className = "text-xs text-[#a4a4a4]">To</h2>
+                <Label className=  "flex-col gap-y-2 flex">
+                    <p className = "text-xs">Workspace</p>
+                    <select
+                        // {...register("workspace_id")}
+                        className = "rounded-xl text-base bg-transparent"
+                    >
+                        <option
+                            className = "text-[#a4a4a4]"
+                            value = "move"
+                        ></option>
+                    </select>
+                </Label>
             </div>
         </form>
     )
